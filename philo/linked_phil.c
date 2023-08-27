8cb262e44757a8350a299a972a99a1f49e1a503f
Yassine Khayri <ykhayri@c1r3p8.1337.ma>
2023-08-27 17:49:31 +0100

    No more leaks

--- philo/linked_phil.c
+++ philo/linked_phil.c
@@ -6,7 +6,7 @@
 /*   By: ykhayri <ykhayri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 20:09:59 by ykhayri           #+#    #+#             */
-/*   Updated: 2023/08/27 16:57:32 by ykhayri          ###   ########.fr       */
+/*   Updated: 2023/08/27 17:48:40 by ykhayri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,4 +68,5 @@ void	no_cash_to_pay(t_single_p **philos)
 		free(*philos);
 		*philos = tmp;
 	}
+	free(*philos);
 }
